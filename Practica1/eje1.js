4766070c006c44f9f1e43f968f0d0b2dfb89dab7
FREDDY <fescobarc@fcpn.edu.bo>
2025-09-22 15:08:20 -0400

    eje1

--- Practica1/eje1.js
+++ Practica1/eje1.js
@@ -1,1 +1,16 @@
-console.log("hola mundo");
+console.log("ESCOBAR CATUNTA FREDDY ELIAS - C.I.6068942 L.P");
+console.log("Carrera INFORMATICA - INF-133 - 24/09/2025");
+function contarVocales(texto = "") {
+  const base = texto.toString().toLowerCase();
+  const contador = { a: 0, e: 0, i: 0, o: 0, u: 0 };
+
+  for (const vocal of base) {
+    if (contador[vocal] !== undefined) {
+      contador[vocal]++;
+    }
+  }
+  return contador;
+}
+console.log("1) Contar vocales en un texto");
+console.log();
+console.log(contarVocales("euforia"));
